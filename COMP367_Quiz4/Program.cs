d9f12d6bde053706432e85ccaba3ff02523e35b8
Parth Shah <parthshah235@gmail.com>
2023-04-14 17:20:06 -0400

    Updated

--- COMP367_Quiz4/Program.cs
+++ COMP367_Quiz4/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine('COMP367 Quiz 4');
+Console.WriteLine("COMP367 Quiz 4");
 Console.WriteLine("Database access has already been done on April 14, 2023 by Parth Shah");
+Console.WriteLine("Cloud connected");

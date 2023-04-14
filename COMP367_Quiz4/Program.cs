1d188289eb9d0aa363381087bcaef3e95f50dc3b
Parth Shah <parthshah235@gmail.com>
2023-04-14 16:53:49 -0400

    Add project files.

---
+++ COMP367_Quiz4/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine('COMP367 Quiz 4');
+Console.WriteLine("Database access has already been done on April 14, 2023 by Parth Shah");
